4f7bbea9a27062401a4d9adcb04edd0a5ee10c6b
Gavin McGruddy <gavin.mcgruddy@tekura.school.nz>
2017-08-29 11:50:03 +1200

    First commit

---
+++ script.js
@@ -0,0 +1,168 @@
+    jokeArray = [
+    '',
+    'Conjunctivitis.com*Now there\'s a site for sore eyes.',
+    'Our wedding was so beautiful...*...even the cake was in tiers.',
+    'I was thinking about moving to Moscow...*...but there\'s no point Russian things.',
+    'When you have a bladder infection...*...urine trouble.',
+    'Without geometry...*...life is pointless.',
+    'I gave all my dead batteries away today...*...free of charge.',
+    'I needed a password eight characters long.*So I picked Snow White and the Seven Dwarfs.',
+    'I\'m so terrified of elevators...*...I think i\'ll start taking steps to avoid them.',
+    'What\'s the advantage of living in Switzerland?*Well the flag is a big plus.',
+    'I\'ve deleted all the Germans I know from my mobile phone. *Now it\'s Hans free.',
+    'I cut my finger chopping cheese.*I think I have grater problems.',
+    'What\s the most common owl?*A teatowel.',
+    'A red and a blue ship collided the other day.*Apparently the survivors are marooned.',
+    'I had a dream about an ocean made out of orange soda.*I think it was a fanta sea.',
+    'What do you call a group of killer whales playing instruments?*An Orca-stra.',
+    'Just watched a documentary about beavers.*Best damn program I\'ve ever seen.',
+    'I\'m worried for the calendar.*I think it\'s days are numbered.',
+    'Did you hear about the new broom that\'s just come out?*Apparently it\'s sweeping the nation.',
+    'I slept like a log last night.*Woke up in the fireplace.',
+    'What cheese can never be yours?*Nacho cheese.',
+    'I hate velcro.*It\'s such a rip-off.',
+    'I went to a zoo which only had one dog in it.*It was a shitzu.',
+    'Why can\'t you hear a pterodactyl go to the bathroom?*The p is silent.',
+    'What do you call a cow with no legs?*Ground beef.',
+    'How did the buffalo say goodbye to his child?*Bison.',
+    'Why did the scarecrow win an award?*He was outstanding in his field.',
+    'Why did the girl smear peanut butter on the road?*To go with the traffic jam.',
+    'Why don\'t seagulls fly over the bay?*Because then they\'d be bagels.',
+    'A man walks into a bar and orders helicopter flavor chips.*The barman replies "Sorry mate, we only do plain."',
+    'Why do scuba divers fall backwards into the water?*Because if they fell forwards they\'d still be in the boat.',
+    'Why did the man quit his can crushing job?*Because it was soda pressing.',
+    'What do you call a deer with no eyes?*No idea.',
+    'I\'d like to give a big shout out to all the sidewalks...*...for keeping me off the streets.',
+    'Where do you learn to make ice cream?*Sunday school.',
+    'What did one molecule say to the other?*I\'ve got my ion you.',
+    'Have you heard of the band 1023MB?*They haven\'t got a GIG yet.',
+    'Did you hear about the guy who invented the knock knock joke?*He won the \'no-bell\' prize.',
+    'Don\'t trust atoms...*...they make up everything.',
+    'I used to hate facial hair...*...but then it grew on me.',
+    'Did you hear about the cheese factory that exploded in France?*There was nothing left but de brie.',
+    'Last night I dreamt I was a muffler.*I woke up exhausted.',
+    'I hate jokes about german sausages.*They\'re the wurst.',
+    'Why couldn\'t the bicycle stand up by itself?*It was two tired.',
+    'What do you call a fake noodle?*An Impasta.',
+    'Wanna hear a joke about paper?*Nevermind, it\'s tearable.',
+    'What do you call a nose without a body?*Nobody knows.',
+    'I\'ve just been diagnosed as colorblind.*It was totally out of the purple.',
+    '"Dad, did you get a haircut?"*"No I got all of them cut."',
+    'What do prisoners use to call each other?*Cell phones.',
+    'Did you hear about the kidnapping at school?*He woke up.',
+    'I love fastening things together with metal.*It\'s so riveting.',
+    'Please don\'t talk to me about drills.*They\'re so boring.',
+    'I just ate a clock.*It was time consuming.',
+    'To the guy who invented zero.*Thanks for nothing.',
+    'How do you find Will Smith in the snow?*Look for the fresh prints.',
+    'Have you heard of the movie \'Constipation\'?*It hasn\'t come out yet.',
+    'What\'s an \'Oying\'?*This joke.',
+    'I stayed up all night wondering where the sun went.*Then it dawned on me.',
+    'I wondered why the frisbee was getting bigger and bigger.*Then it hit me.',
+    'I just read about a dwarf that was pickpocketed.*I can\'t believe someone could stoop so low.',
+    'My bike keeps throwing me off.*It\'s a vicious cycle.',
+    'I\'ve just learned sign language.*It\'s pretty handy.',
+    'I\'m always suspicious of trees.*They always look shady to me.',
+    'Yesterday a clown held a door open for me.*I thought it was a nice jester.',
+    'When you touch something that causes an allergic reaction...*...try not to make any rash decisions.',
+    'A friend of mine tried to annoy me with bird puns...*...but I soon realized that toucan play at that game.',
+    'I used to be a banker...*...but I lost interest.',
+    'I couldn\'t quite remember how to throw a boomerang...*...but eventually it came back to me.',
+    'Show me a piano falling down a mineshaft...*...and I\'ll show you A-flat minor.',
+    'A man just assaulted me with milk, cream and butter.*How dairy.',
+    'When Peter Pan punches...*...they Neverland.',
+    'I did a theatrical performance about puns.*Really it was just a play on words.',
+    'What happened when the butcher backed into the meat grinder?*He got a little behind in his work.',
+    'If towels could tell jokes...*...they would have a dry sense of humor.',
+    'Did you hear about the crime that happened in a parking garage?*It was wrong on so many levels.',
+    'Did you hear about these new reversible jackets?*I\'m excited to see how they turn out.',
+    'There was a big paddle sale at the boat store.*It was quite an oar deal.',
+    'My tailor is happy to make a pair of pants for me...*...or at least sew it seams.',
+    'I took a bad picture of a field of wheat...*...it was too grainy.',
+    'I should have been sad when my flashlight batteries died...*...but I was delighted.',
+    'I try wearing tight jeans...*...but I can never pull them off.',
+    'England doesn\'t have a kidney bank...*...but it does have a Liverpool.',
+    'Time flies like an arrow.*Fruit flies like a banana.',
+    'What to call a bear who\'s lost all its teeth?*A gummy bear.',
+    'I accidentally ate some scrabble pieces last night.*I fear my next poop could spell disaster.',
+    'What did the Tin Man say when he got run over by a steamroller?*"Foiled again!"',
+    'Where are average things manufactured?*The satisfactory.',
+    'How do you drown a Hipster?*Throw him into the mainstream.',
+    'Did you hear about the mathematician who\'s afraid of negative numbers?*He will stop at nothing to avoid them.',
+    'What do you call a number that can\'t keep still?*A roamin\' numeral.',
+    'What did Al Gore play on his guitar?*An Algorithm.',
+    'What do you call a parade of rabbits hopping backwards?*A receding hare-line.',
+    'Why should you never breakup with a goalie?*Because he\'s a keeper.',
+    'Why can\'t you explain puns to kleptomaniacs?*They always take things literally.',
+    'A man entered 10 pun contests hoping one joke would win.*Unfortunately, no pun in ten did.',
+    'What did the ill comic say in the hospital?*"I\'m here all weak!"',
+    'After the birth of your first child...*...your role in life will become apparent.',
+    'Theyre finally making a movie called clocks.*It\'s about time.',
+    'I saw a sign that said falling rocks...*...so I tried and it doesn\'t.'
+    ],
+
+    randomIndex = Math.floor( Math.random() * jokeArray.length );
+    previousIndex = 0;
+    animationIndex = 0,
+    previousAnimationIndex = 0;
+    timeOut1 = 500;
+    timeOut2 = 0;
+    timeOut3 = 0;
+    previousJokes = new Array();
+
+    $(document).on('ready', function() {
+
+    	$('#coffee-stain-container').on('click', function(event){
+
+            event.preventDefault();
+
+            $('#joke').css('display', 'none');
+            $('#punchline').css('display', 'none');
+            $('#coffee-stain-container').css('display', 'none');
+
+        	while(jQuery.inArray( randomIndex, previousJokes) !== -1 || randomIndex === 0) {
+               randomIndex = Math.floor( Math.random() * jokeArray.length );
+        	}
+
+        	previousJokes.push(randomIndex);
+
+        	var joke = jokeArray[randomIndex].split("*");
+
+        	$('#joke').text(joke[0]);
+        	$('#punchline').text(joke[1]);
+
+            timeOut2 = joke[0].length * 65 + timeOut1;
+
+            if(timeOut2 < 3000) {
+                timeOut2 = 3000;
+            }
+
+            timeOut3 = joke[1].length * 65 + timeOut2;
+
+            if(timeOut3 < 5600) {
+                timeOut3 = 5600;
+            }
+
+        	setTimeout(function(){
+                $('#joke').fadeIn('slow');
+            }, timeOut1);
+
+            setTimeout(function(){
+                $('#punchline').fadeIn('slow');
+            }, timeOut2);
+
+            setTimeout(function(){
+                $('#coffee-stain-container').fadeIn('fast');
+            }, timeOut3);
+
+            randomIndex = 0;
+
+            var adjJokeLength = jokeArray.length - 1;
+
+            if(previousJokes.length == adjJokeLength) {
+                previousJokes = [];
+            }
+
+    	})
+
+});
